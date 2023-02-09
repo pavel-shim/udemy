d47152fe39dccc46de385f06ae253f1768b13d93
pave-shima <p3757494@gmail.com>
2023-02-09 22:44:12 +0300

    dz for lesson 29

--- script.js
+++ script.js
@@ -385,3 +385,80 @@ console.log(Math.round(num));
 
 const test = "12.2px"
 console.log(parseInt(test));
+
+// lesson 29 dz
+
+function calculateVolumeAndArea(a) {
+
+    if (Number.isInteger(a) == false || typeof a != "number" || a < 0) {
+        const error = 'При вычислении произошла ошибка';
+        console.log(error)
+        return error;
+    } else {
+        const V = a * a * a; 
+        const S = 6 * a * a;
+        const result = `Объем куба: ${V}, площадь всей поверхности: ${S}`;
+        console.log(result)
+        return result;
+    }
+    
+}
+
+calculateVolumeAndArea(0);
+
+
+function getCoupeNumber(a) {
+
+    if (Number.isInteger(a) == false || typeof a != "number" || a < 0) {
+        const error = "Ошибка. Проверьте правильность введенного номера места";
+        console.log(error);
+        return error;
+    }  else if (a == 0 || a > 36) {
+        const noPlace = "Таких мест в вагоне не существует";
+        console.log(noPlace);
+        return noPlace;
+    }  else {
+        const result = Math.ceil(a / 4);
+        console.log(result);
+        return result;
+    } 
+
+}
+
+getCoupeNumber(0);
+
+//dz lesson 29 результат учителя
+
+function calculateVolumeAndArea(length) {
+    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+        return "При вычислении произошла ошибка";
+    }
+
+    let volume = 0,
+        area = 0;
+
+    volume = length * length * length;
+    // length ** 3 - это тоже самое, что и выше или варианты через цикл.
+    // ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает =/
+    area = 6 * (length * length);
+
+    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+}
+
+calculateVolumeAndArea(5);
+
+function getCoupeNumber(seatNumber) {
+    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    return Math.ceil(seatNumber / 4);
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
+}
+
+getCoupeNumber(33);
+
